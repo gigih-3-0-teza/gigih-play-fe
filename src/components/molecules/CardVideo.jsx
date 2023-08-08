0483c3183cc55471d1db91940c0807d204dcc506
Teza Alfian <44139001+tezaalfian@users.noreply.github.com>
2023-08-09 06:59:50 +0700

    refactoring

--- src/components/molecules/CardVideo.jsx
+++ src/components/molecules/CardVideo.jsx
@@ -6,7 +6,7 @@ export default function CardVideo({ title, description, thumbnail }) {
             }}>
             <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
             <div className="z-10">
-                <h1 className="text-teal-500 text-xl line-clamp-1 mb-1">{title}</h1>
+                <h1 className="text-teal-500 text-xl mb-1">{title}</h1>
                 <small className="text-sm line-clamp-2">{description}</small>
             </div>
         </div>
